44ea83f76f462fb59821e13b34c37d43955442c3
Gianna-Carina Gruen <gcgruen@gmail.com>
2025-05-23 08:21:39 +0200

    Update UNCHR-Arrivals-Europe.py

--- UNCHR-Arrivals-Europe.py
+++ UNCHR-Arrivals-Europe.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 from datawrapper import Datawrapper
+from bs4 import BeautifulSoup
 
 dw = Datawrapper(access_token = "YOUR TOKEN HERE") #todo: add your own datawrapper API token
 
@@ -48,6 +49,13 @@ chart_id='' #todo: add your own chart ID here
 
 latest_data_from_month = df_merged_selected.timestamp.max()
 
+PAGE_URL='https://data.unhcr.org/en/situations/europe-sea-arrivals'
+response = requests.get(PAGE_URL) # get the date from the page
+pagehtml = BeautifulSoup(response.text, "html.parser") # parse the html
+
+date_str = pagehtml.find_all('div', {'class':'popTotalDate'}) # find all divs with this class (inspect page via console)
+date_str_tidy = date_str[0].text.strip().replace('Last updated ', '') #pick the first element, tidy it up a bit
+
 latest_year = df_merged.year.max()
 
 total_arrivals_latest_year = df_merged[df_merged['year']==latest_year]['individuals_land+sea'].sum()
@@ -56,7 +64,7 @@ total_arrivals_latest_year_formatted = f"{total_arrivals_latest_year:,}"
 dw.update_description(
  chart_id,
  intro=f'Total arrivals of individuals since the beginning of this year so far: {total_arrivals_latest_year_formatted}',
- source_name = f'UNHCR, latest data from: {latest_data_from_month}',
+ source_name = f'UNHCR, latest data from: {date_str_tidy}',
  source_url = 'https://data.unhcr.org/en/situations/europe-sea-arrivals',
 )
 
